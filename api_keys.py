810de9d0e01c9996fc4a77449092f67aabcde469
Joe Cross <joe.mcross@gmail.com>
2013-04-25 13:22:49 -0700

    api_keys SQLAlchemy -> MongoDB
    Replace all SQLAlchemy calls with PyMongo calls.
    remove_primary_key can remove derived keys
    derive_key stores salt with derived key
    validate_key allows primary keys.  No expiration means valid forever

--- api_keys.py
+++ api_keys.py
@@ -1,86 +1,78 @@
-import boto.dynamodb
-
-
 from mtg_search.util import timeutil
 from mtg_search.pbkdf2 import pbkdf2
 from mtg_search import app
 
 DERIVED_KEY_LIFETIME = int(app.config['DERIVED_KEY_LIFETIME'])
 DERIVED_KEY_LENGTH = int(app.config['DERIVED_KEY_LENGTH'])
-
-_conn = boto.dynamodb.connect_to_region(
-    app.config['DYNAMODB_REGION'],
-    aws_access_key_id=app.config['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=app.config['AWS_SECRET_ACCESS_KEY']
-)
-dk_table = _conn.get_table(app.config['DERIVED_KEY_TABLE'])
-pk_table = _conn.get_table(app.config['PRIMARY_KEY_TABLE'])
+PRIMARY = 'primary'
+DERIVED = 'derived'
 
 
-def _get_primary_key(key):
-    try:
-        return pk_table.get_item(hash_key=key)
-    except boto.dynamodb.exceptions.DynamoDBKeyNotFoundError:
-        return None
+def _get_primary_key(keys, key):
+    return keys.find_one({'key': key, 'type': PRIMARY})
 
 
-def has_primary_key(key):
-    return bool(_get_primary_key(key))
+def has_primary_key(keys, key):
+    return bool(_get_primary_key(keys, key))
 
 
-def add_primary_key(key):
-    if not has_primary_key(key):
-        pk_table.new_item(hash_key=key).put()
+def add_primary_key(keys, key):
+    if not has_primary_key(keys, key):
+        key = {'key': key, 'type': PRIMARY}
+        keys.insert(key)
 
 
-def remove_primary_key(key):
-    item = _get_primary_key(key)
-    if item:
-        item.delete()
+def remove_primary_key(keys, key, remove_derived=True):
+    primary = _get_primary_key(keys, key)
+    if not primary:
+        return
+    keys.remove(primary)
+
+    if not remove_derived:
+        return
+    for derived_key in keys.find({'type': DERIVED}):
+        computed_key, salt = pbkdf2(key, salt=derived_key['salt'])
+        if computed_key == derived_key['key']:
+            keys.remove(derived_key)
 
 
-def list_primary_keys():
-    return [i['key'] for i in pk_table.scan()]
+def list_primary_keys(keys):
+    return [i['key'] for i in keys.find({'type': PRIMARY})]
 
 
-def derive_key(key, duration=DERIVED_KEY_LIFETIME):
-    invalidate_derived_key(key)
-    utc_expiration = timeutil.utcnow(minutes=duration)
+def list_derived_keys(keys):
+    return [i['key'] for i in keys.find({'type': DERIVED})]
+
+
+def derive_key(keys, key, duration=DERIVED_KEY_LIFETIME):
+    if not has_primary_key(keys, key):
+        return None, None
+    utc_expiration = timeutil.utcnow(seconds=duration)
     expiration = timeutil.utc_asint(utc_expiration)
     derived_key, salt = pbkdf2(key, key_len=DERIVED_KEY_LENGTH)
-    item = dk_table.new_item(
-        hash_key=derived_key,
-        attrs={
-            'e': expiration,
-            's': salt
-        }
-    )
-    item.put()
+    key = {'key': derived_key, 'salt': salt, 'type': DERIVED, 'exp': expiration}
+    keys.insert(key)
     return derived_key, salt
 
 
-def invalidate_derived_key(primary_key):
-    for item in dk_table.scan():
-        derived_key = item['key']
-        salt = item['s']
-        calculated_key, salt = pbkdf2(primary_key, key_len=DERIVED_KEY_LENGTH, salt=salt)
-        if calculated_key == derived_key:
-            item.delete()
-
-
-def validate_derived_key(key):
-    expiration = get_derived_key_expiration(key)
-    if not expiration:
+def validate_key(keys, key):
+    key = keys.find_one({'key': key})
+    if not key:
         return False
+    expiration = key.get('exp', None)
+    if not expiration:
+        return True
     now = timeutil.utc_asint(timeutil.utcnow())
     if now > expiration:
-        dk_table.get_item(hash_key=key).delete()
+        keys.remove(key)
         return False
     return True
 
 
-def get_derived_key_expiration(key):
-    try:
-        return dk_table.get_item(hash_key=key)['e']
-    except boto.dynamodb.exceptions.DynamoDBKeyNotFoundError:
-        return None
+def get_derived_key(keys, key):
+    return keys.find_one({'key': key, 'type': DERIVED})
+
+
+def get_derived_key_expiration(keys, key):
+    key = get_derived_key(keys, key)
+    return key['exp'] if key else None
