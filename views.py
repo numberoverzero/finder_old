63113d7007c68d6b26d5e49ab69e840484f10114
Joe Cross <joe.mcross@gmail.com>
2013-04-25 13:20:05 -0700

    views use updated api_keys functions
    minor name changes, keys() function loads correct MongoDB Collection for
    key validation

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from flask.ext.restful import Resource, fields, marshal_with, abort
 from mtg_search import app, api, mongo
-from api_keys import derive_key, validate_derived_key, get_derived_key_expiration
+from api_keys import derive_key, validate_key, get_derived_key_expiration
 from mtg_search.util import timed
 
 
@@ -12,13 +12,19 @@ gen_fields = {
 }
 
 
-class DerivedKeyGenerate(Resource):
+def keys():
+    return getattr(mongo.db, app.config['KEYS_COLL_NAME'])
+
+
+class GenerateDerivedKey(Resource):
     @marshal_with(gen_fields)
     def get(self, base_key):
-        derived_key, salt = derive_key(base_key)
-        expiration = get_derived_key_expiration(derived_key)
+        derived_key, salt = derive_key(keys(), base_key)
+        if not derived_key:
+            abort(403, message="Unauthenticated call.")
+        expiration = get_derived_key_expiration(keys(), derived_key)
         return {'key': derived_key, 'salt': salt, 'expiration': expiration}
-api.add_resource(DerivedKeyGenerate, '/generate/<string:base_key>')
+api.add_resource(GenerateDerivedKey, '/generate/<string:base_key>')
 
 
 validate_fields = {
@@ -31,8 +37,8 @@ validate_fields = {
 class ValidateKey(Resource):
     @marshal_with(validate_fields)
     def get(self, derived_key):
-        valid = validate_derived_key(derived_key)
-        expiration = get_derived_key_expiration(derived_key)
+        valid = validate_key(keys(), derived_key)
+        expiration = get_derived_key_expiration(keys(), derived_key)
         return {'key': derived_key, 'valid': valid, 'expiration': expiration}
 api.add_resource(ValidateKey, '/validate/<string:derived_key>')
 
@@ -42,7 +48,7 @@ def authenticated(func):
     def wrapper(*args, **kwargs):
         with timed(lambda d: app.logger.info("Checked key in {} seconds".format(d))):
             api_key = kwargs.pop('api_key', None)
-            valid = validate_derived_key(api_key)
+            valid = validate_key(keys(), api_key)
         if not valid:
             abort(403, message="Unauthenticated call.")
         return func(*args, **kwargs)
@@ -52,7 +58,7 @@ def authenticated(func):
 class GetCardIds(Resource):
     @authenticated
     def get(self, card_name):
-        # Use mongo.db.cards
+        # Use keys().db.cards
         return {'name': card_name, 'ids': ['A', 'B']}
 
 api.add_resource(GetCardIds, '/<string:api_key>/ids/<string:card_name>')
