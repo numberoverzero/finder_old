4fc4ebf1bb21b649d6f80ea281c40c66ed24d99e
Joe Cross <joe.mcross@gmail.com>
2013-04-25 13:18:37 -0700

    Add clean function to apikeys util
    This cleans up any expired derived keys

--- scripts/apikeys.py
+++ scripts/apikeys.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from mtg_search.api_keys import add_primary_key, remove_primary_key, list_primary_keys
+from mtg_search.api_keys import add_primary_key, remove_primary_key, list_primary_keys, list_derived_keys, validate_key
+from mtg_search.scripts import mongo, config
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -13,17 +14,29 @@ parser_add.add_argument('key', help='The new base api key to add')
 # Remove Key
 parser_remove = subparsers.add_parser('remove')
 parser_remove.add_argument('key', help='The base api key to remove')
+parser_remove.add_argument('-d', '--derived_keys', help='Also remove all keys derived from this base key', action='store_true')
 
 # List Keys
 parser_list = subparsers.add_parser('list')
 
+# Clean up expired derived keys
+parser_list = subparsers.add_parser('clean')
+
 args = parser.parse_args()
 cmd = args.subcommand
 
+
+def keys():
+    return getattr(mongo.db, config['KEYS_COLL_NAME'])
+
+
 if cmd == 'add':
-    add_primary_key(args.key)
+    add_primary_key(keys(), args.key)
 elif cmd == 'remove':
-    remove_primary_key(args.key)
+    remove_primary_key(keys(), args.key, args.derived_keys)
 elif cmd == 'list':
-    for key in list_primary_keys():
+    for key in list_primary_keys(keys()):
         print key
+elif cmd == 'clean':
+    for key in list_derived_keys(keys()):
+        validate_key(keys(), key)
