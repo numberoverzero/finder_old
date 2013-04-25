e5da4c5cf0b66c7ea01402206c2853e771695975
Joe Cross <joe.mcross@gmail.com>
2013-04-25 12:52:11 -0700

    pbkdf2 helper forces salt ascii

--- pbkdf2/__init__.py
+++ pbkdf2/__init__.py
@@ -8,6 +8,6 @@ def _salt(len=16):
 
 def pbkdf2(key, key_len=12, salt=None):
     key = key.encode('ascii', 'ignore')
-    salt = salt or _salt(64)
+    salt = (salt or _salt(64)).encode('ascii', 'ignore')
     dkey = pbkdf2_hex(key, salt, iterations=10000, keylen=key_len)
     return dkey, salt
